28338cc553327f8943e63fc886054f5d362bdc6c
Aidan Chang <aidan.chang@outlook.com>
2026-02-27 00:41:38 -0600

    move countdown to top right

--- content.js
+++ content.js
@@ -25,13 +25,15 @@ function showCountdown(endTime) {
     const countdownDiv = document.createElement('div');
     countdownDiv.id = 'countdownDiv';
     countdownDiv.style.position = 'fixed';
-    countdownDiv.style.top = '10px';
-    countdownDiv.style.right = '10px';
-    countdownDiv.style.padding = '15px';
+    countdownDiv.style.lineHeight = 'normal';
+    countdownDiv.style.top = '20px';
+    countdownDiv.style.left = '20px';
+    countdownDiv.style.padding = '25px';
     countdownDiv.style.backgroundColor = 'red';
     countdownDiv.style.color = 'white';
-    countdownDiv.style.fontSize = '45px';
+    countdownDiv.style.fontSize = '65px';
     countdownDiv.style.zIndex = '2147483647';
+    countdownDiv.style.fontFamily = 'Segoe UI, sans-serif';
     countdownDiv.style.borderRadius = '5px';
     document.body.appendChild(countdownDiv);
 
